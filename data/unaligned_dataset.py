c2e4abc7f29c2888059bb19ccd93f74b93e3d58d
kashyappiyush1998 <kashyappiyush1998@gmail.com>
2021-03-09 11:02:30 +0530

    image folder

--- data/unaligned_dataset.py
+++ data/unaligned_dataset.py
@@ -96,6 +96,7 @@ class UnalignedDataset(BaseDataset):
 
         
         if self.opt.resize_or_crop == 'no':
+            print(self.opt.resize_or_crop)
             r,g,b = A_img[2]+1, A_img[1]+1, A_img[0]+1
             A_gray = 1. - (0.299*r+0.587*g+0.114*b)/2.
             A_gray = torch.unsqueeze(A_gray, 0)
